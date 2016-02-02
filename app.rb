92c214d2a8680bf069227897a9c8c3d530ecab81
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-02-02 10:52:45 +0000

    added params

--- app.rb
+++ app.rb
@@ -6,10 +6,15 @@ end
 
 get '/secret' do
   "Secret message"
-end 
+end
 
-get '/cat' do 
-	"<div style='border: 3px dashed red'>
-	<img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
-	</div>"
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
